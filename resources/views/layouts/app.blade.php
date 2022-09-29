051626b462304d77680d9d0d2523856a69e17a3d
hk-daichi <hk-daichi@outlook.jp>
2022-09-29 21:02:16 +0900

    bladeファイル修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Match×Match') }}</title>
+    <title>Match×Match</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -30,7 +30,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     Match×Match
-                    <!--{{ config('app.name', 'Laravel') }}-->
+                    <!--{{ config('app.name', 'Match×Match') }}-->
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
